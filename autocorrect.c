7f54dc7c2ea10bf3e63737007afdd66866a6dd78
BrunoBaldissera <38329588+BrunoBaldissera@users.noreply.github.com>
2018-10-02 01:52:44 -0300

    Add files via upload

--- autocorrect.c
+++ autocorrect.c
@@ -9,31 +9,31 @@ struct node_{
 };
 
 typedef struct node_ NODE;
-    
-/*Aqui √© criado um novo n√≥ a ser inserido na trie,
+
+/*Aqui È criado um novo nÛ a ser inserido na trie,
   inicializando todos os seus ponteiros 'filhos' como NULO
-  e atribuindo 0 ao valor 'fim' -que indica se o n√≥ representa
-  o fim de uma palara ou n√£o - do n√≥*.
+  e atribuindo 0 ao valor 'fim' -que indica se o nÛ representa
+  o fim de uma palara ou n„o - do nÛ*.
   ------------------------------------------------------------
-  A fun√ß√£o recebe como par√¢metro apenas o ponteiro para o n√≥ 'n'
-  que ser√° criado, e o retorna j√° alocado e com suas devidas
-  altera√ß√µes*/  
+  A funÁ„o recebe como par‚metro apenas o ponteiro para o nÛ 'n'
+  que ser· criado, e o retorna j· alocado e com suas devidas
+  alteraÁıes*/
 NODE* create_node(NODE* n){
 	n = (NODE*) malloc(sizeof(NODE));
 	n->end = 0;
-	for(int i = 0; i < 26; i++){ /*Atribui todos os n√≥s 'filhos' do n√≥ criado para NULO*/
+	for(int i = 0; i < 26; i++){ /*Atribui todos os nÛs 'filhos' do nÛ criado para NULO*/
 		n->kid[i] = NULL;
 	}
 	return n;
 }
 
-/*Esta fun√ß√£o dever√° receber cada palavra recebida a partir da leitura do livro
-  que serve de base para a cria√ß√£o do dicion√°rio na fun√ß√£o 'read_book' e inseri-la
-  na estrutura de dados Trie, efetuando todas as verifica√ß√µes necess√°rias para que n√£o
-  haja desperd√≠cio ou repeti√ß√µes de palavras.
+/*Esta funÁ„o dever· receber cada palavra recebida a partir da leitura do livro
+  que serve de base para a criaÁ„o do dicion·rio na funÁ„o 'read_book' e inseri-la
+  na estrutura de dados Trie, efetuando todas as verificaÁıes necess·rias para que n„o
+  haja desperdÌcio ou repetiÁıes de palavras.
   ------------------------------------------------------------------------------------
-  A fun√ß√£o recebe como par√¢metro a string est√°tica 'word' e o ponteiro para o 
-  n√≥ ra√≠z da trie. N√£o se retorna nada.*/
+  A funÁ„o recebe como par‚metro a string est·tica 'word' e o ponteiro para o
+  nÛ raÌz da trie. N„o se retorna nada.*/
 
 void insert_word(char word[], NODE* root){
 	//printf("insert word function called\n");
@@ -42,20 +42,20 @@ void insert_word(char word[], NODE* root){
 	//printf("length of '%s' = %d\n", word, len);
 	int pos = 0;
 	for(int i = 0; i < len; i++){
-		word[i] = tolower(word[i]); /*trataremos todas as letras como caracteres min√∫sculos*/
-		pos = word[i] - 'a'; /*pos ser√° um n√∫mero entre 0 e 25 que representa uma letra do alfabeto*/
+		word[i] = tolower(word[i]); /*trataremos todas as letras como caracteres min˙sculos*/
+		pos = word[i] - 'a'; /*pos ser· um n˙mero entre 0 e 25 que representa uma letra do alfabeto*/
 		//printf("value of letter '%c' is %d\n", word[i], pos);
 		if (aux->kid[pos] == NULL) aux->kid[pos] = create_node(aux);
 		aux = aux->kid[pos];
 	}
 	if (aux != root) aux->end = 1;
 }
-/*enquanto o arquivo n√£o acabar, n√≥s leremos todo conjunto de caracteres
-  encontrado at√© um diferente de a - Z como string e a chamamos de 'word'. Em seguida,
-  chamamos a fun√ß√£o 'insert_word()' para inserir essa palavra na estrutura Trie, que
-  servir√° como o dicion√°rio deste programa
+/*enquanto o arquivo n„o acabar, nÛs leremos todo conjunto de caracteres
+  encontrado atÈ um diferente de a - Z como string e a chamamos de 'word'. Em seguida,
+  chamamos a funÁ„o 'insert_word()' para inserir essa palavra na estrutura Trie, que
+  servir· como o dicion·rio deste programa
   ------------------------------------------------------------------------------------
-  A fun√ß√£o n√£o recebe par√¢metros e tem como retorno o ponteiro para o n√≥ ra√≠z da trie*/
+  A funÁ„o n„o recebe par‚metros e tem como retorno o ponteiro para o nÛ raÌz da trie*/
 NODE* read_book(void){
 	NODE* root = NULL;
 	root = create_node(root);
@@ -67,7 +67,7 @@ NODE* read_book(void){
 	fp = fopen(filename, "r");
 	//printf("book file opened\n");
 	char word[46];
-	
+
 	while(!feof(fp)){
 		fscanf(fp, "%[a-zA-Z]%*[^a-zA-Z]", word);
 		//printf("word scanned: %s\n", word);
@@ -77,9 +77,25 @@ NODE* read_book(void){
 	return root;
 }
 
-void check_and_print_error(char tword[], NODE* root){
-	//verifica se a palavra atual existe na trie
-	//se n√£o existir, imprime a palavra do pr√≥prio tweet seguida de espa√ßo
+void check_and_print_error(char* tword, NODE* root){
+	NODE* aux = root;
+	int len = strlen(tword);
+	char lowerword[len];
+	strcpy(lowerword, tword);
+	int pos = 0;
+	for(int i = 0; i < len; i++){
+        lowerword[i] = tolower(lowerword[i]);
+        pos = lowerword[i] - 'a';
+        if (aux->kid[pos] == NULL){
+            printf("%s ", tword);
+            break;
+        }
+        if (i == (len-1) && aux->end == 1){
+            printf("%s ", tword);
+            break;
+        }
+        aux = aux->kid[pos];
+	}
 }
 
 void read_tweets(NODE* root){
@@ -93,23 +109,21 @@ void read_tweets(NODE* root){
 	char tweet[281];
 	char tword[281];
 	int len = 0, count = 0, lenaux = 0;
-	while(!feof(ft)){	
+	while(!feof(ft)){
 		fscanf(ft, "%s", findtxt);
 		if (strncmp(findtxt, "\"text\":", 7) == 0){
 			printf("text was found: %s\n", findtxt);
 			fscanf(ft, " \"");
 			fscanf(ft, " %[^\"]", tweet);
 			printf("%s\n", tweet);
-			len = strlen(tweet);
-			while(count < len){
-				sscanf(tweet, "%[a-zA-Z]%*[^a-zA-Z]", tword);
-				lenaux = strlen(tword);
-				printf("the tweet word is '%s'\n", tword);
-				//check_and_print_error(tword, root);
-				count++;
+			char* tword = strtok(tweet, ".,!?\"\n&...íí;@-ìî#");
+			while(tword != NULL){
+                tword = strtok(NULL, ".,!?\"\n&...íí;@-ìî#");
+                printf("the tokenized word was '%s'\n", tword);
+				check_and_print_error(tword, root);
 			}
-			printf("\n");				
-		}	
+			printf("\n");
+		}
 	}
 }
 int main(int argc, char* argv[]){
