4b5d402acef0a9be6dac98316efe212fb37e6b90
BrunoBaldissera <38329588+BrunoBaldissera@users.noreply.github.com>
2018-11-03 21:05:33 -0300

    Add files via upload

--- abas.c
+++ abas.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define DEBUG 1
+
 typedef struct{
 	int mes;
 	int dia;
@@ -12,14 +14,13 @@ typedef struct{
 typedef struct{
 	char titulo[32];
 	char url[1024];
-	Data* data;
-	int hora;
+	Data data;
+	int pos;
 }Aba;
 
 typedef struct no{
 	struct no* prox;
-	struct no* ante;
-	Aba* aba;
+	Aba aba;
 }No;
 
 typedef struct{
@@ -27,49 +28,75 @@ typedef struct{
 	No* fim;
 	int tam;
 }Lista;
-  
-void abrir(){
-	No* novo = (No*) malloc(sizeof(No));
-	scanf("%");
-	scanf("");
-	scanf("");
-	scanf("");
+
+No* cria_no(void){
+	No* n = (No*) malloc(sizeof(No));
+	n->prox = NULL;
+	return n;
+}  
+
+Lista* cria_lista(void){
+	Lista* L = (Lista*) malloc(sizeof(Lista));
+	L->cabeca = cria_no();
+	L->fim = L->cabeca;
+	L->tam = 0;
+	if (DEBUG) printf("Lista criada com sucesso\n");
+	return L;
+}
+ 
+void abrir(Lista* L){
+	if (DEBUG) printf ("operação de abrir nova aba inicializada\n");
+	No* novo = cria_no();
+	scanf("%[^\n]%*c", novo->aba.titulo);
+	scanf("%[^\n]%*c", novo->aba.url);
+	scanf("%d %d", &novo->aba.data.dia, &novo->aba.data.mes);
+	scanf("%d %d", &novo->aba.data.hora, &novo->aba.data.minuto);
+	
+	L->fim->prox = novo;
+	novo->prox = NULL;
+	L->fim = novo;
+	(L->tam)++;
+	if (DEBUG) printf ("operação de abrir nova aba finalizada\n");
 }
 
-void alterar(){
-
+void alterar(Lista* L){
+	if (DEBUG) printf ("função \"alterar\" chamada\n");
 }
 
-void ordenar(){
-
+void ordenar(Lista* L){
+	if (DEBUG) printf ("função \"ordenar\" chamada\n");
 }
 
-void exibir(){
-
+void exibir(Lista* L){
+	if (DEBUG) printf ("função \"exibir\" chamada\n");
 }
 
-void sair(){
+void sair(Lista* L){
+	if (DEBUG) printf ("função \"sair\" chamada\n");
 	//free;
 }
 
 int main(int argc, char* argv[]){
 	int escolha;
+	Lista* L = cria_lista();
 	do{
+		if (DEBUG) printf("insira o valor correspondente à operação desejada\n");
 		scanf("%d", &escolha);
+		getchar();
 		switch (escolha){
 			case 1:
-				abrir();
+				abrir(L);
 				break;
 			case 2:
-				alterar();
+				alterar(L);
 				break;
 			case 3:
-				ordenar();
+				ordenar(L);
 				break;
 			case 4:
-				exibir();
+				exibir(L);
 		 }
 	}while(escolha != 5);
-	sair();
+	sair(L);
 return 0;
 }
